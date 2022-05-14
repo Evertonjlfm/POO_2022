ac0ca17dc37af8b04b504316f30a631a6c6880f1
Éverton Júlio <91695422+Evertonjlfm@users.noreply.github.com>
2022-05-14 23:23:02 +0000

    Resolvido exercicio01 da Lista02

--- Lista02/Exercicio01.cs
+++ Lista02/Exercicio01.cs
@@ -2,6 +2,8 @@ using System;
 
 class Program {
   public static void Main(string[] args) {
-    Console.WriteLine("Évsdasdad");
+    Console.Write("Digite seu primeiro nome: ");
+    string nome = Console.ReadLine();
+    Console.WriteLine($"Bem vindo(a) ao C#, {nome}.");
   }
 }
