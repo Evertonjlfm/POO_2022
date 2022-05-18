a6f8d4a1511f81e42bde5048cb7c8ea4a2f05cfe
Éverton Júlio <91695422+Evertonjlfm@users.noreply.github.com>
2022-05-18 14:43:44 +0000

    Adicionado(com erro) exercico 1018.cs da Lista03.

---
+++ Lista03/uri1018.cs
@@ -0,0 +1,58 @@
+using System;
+
+class Program{
+  public static void Main(string[] args){
+    int c100 = 0;int c50  = 0;int c20 = 0;int c10 = 0;int c5 = 0;int c2 = 0;int c1 = 0;
+    int n = int.Parse(Console.ReadLine());
+    if(n >= 100){
+      while(n/100 != 0){
+          n-=100;
+          c100++;
+        }
+    }
+    if(n >= 50){
+      while(n/50 != 0){
+          n-=50;
+          c50++;
+        }
+    }
+    if(n >= 20){
+      while(n/20 != 0){
+          n-=20;
+          c20++;
+        }
+    }
+    if(n >= 10){
+      while(n/10 != 0){
+          n-=10;
+          c10++;
+        }
+    }
+    if(n >= 5){
+      while(n/5 != 0){
+          n-=5;
+          c5++;
+        }
+    }
+    if(n >= 2){
+      while(n/2 != 0){
+          n-=2;
+          c2++;
+        }
+    }
+    if(n >= 1){
+      while(n/1 != 0){
+          n-=1;
+          c1++;
+        } 
+    }
+    Console.WriteLine(c100 + " nota(s) de R$ 100,00");
+    Console.WriteLine(c50 + " nota(s) de R$ 50,00");
+    Console.WriteLine(c20 + " nota(s) de R$ 20,00");
+    Console.WriteLine(c10 + " nota(s) de R$ 10,00");
+    Console.WriteLine(c5 + " nota(s) de R$ 5,00");
+    Console.WriteLine(c2 + " nota(s) de R$ 2,00");
+    Console.WriteLine(c1 + " nota(s) de R$ 1,00"); 
+  }
+}
+      
